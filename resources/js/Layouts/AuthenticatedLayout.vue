8cd989fd576e2e031702ac12a72bf6eca729cd54
koich505 <k.09034514347@gmail.com>
2023-04-04 12:37:23 +0900

    商品関連

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -5,7 +5,8 @@ import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import NavLink from "@/Components/NavLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
-import { Link } from "@inertiajs/vue3";
+//import { Link } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/inertia-vue3";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -36,6 +37,12 @@ const showingNavigationDropdown = ref(false);
                                     :active="route().current('dashboard')"
                                 >
                                     Dashboard
+                                </NavLink>
+                                <NavLink
+                                    :href="route('items.index')"
+                                    :active="route().current('items.index')"
+                                >
+                                    商品管理
                                 </NavLink>
                             </div>
                         </div>
@@ -143,6 +150,12 @@ const showingNavigationDropdown = ref(false);
                             :active="route().current('dashboard')"
                         >
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            :href="route('items.index')"
+                            :active="route().current('items.index')"
+                        >
+                            商品管理
                         </ResponsiveNavLink>
                     </div>
 
